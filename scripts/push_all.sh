a6c016848847187b2ced7c1d9107fa25bfc41af5
Andy Huang <andy@{domain}>
2020-04-27 13:28:17 -0400

    Add L1 srcs to push script
    Co-authored-by: Andy Huang <ahuang-noreply@protonmail.com>

--- scripts/push_all.sh
+++ scripts/push_all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-mapfile -t GIT_REMOTES < <(jq -r "keys[]" L0_srcs)
+mapfile -t GIT_REMOTES_0 < <(jq -r "keys[]" L0_srcs)
+mapfile -t GIT_REMOTES_1 < <(jq -r "keys[]" L1_srcs)
 
-for remote in "${GIT_REMOTES[@]}"; do
+for remote in "${GIT_REMOTES_0[@]}"; do
+	git push $remote master "$@"
+done
+
+for remote in "${GIT_REMOTES_1[@]}"; do
 	git push $remote master "$@"
 done
